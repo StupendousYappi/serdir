bb68183c8eb7e6483659926c72e6a72224760756
Greg Steffensen <greg.steffensen@gmail.com>
2026-02-07 09:10:04 -0500

    Adding debug info to fix broken windows test.

--- src/platform.rs
+++ src/platform.rs
@@ -168,6 +168,7 @@ mod test {
             .unwrap();
         // This should fail because it's not open for reading.
         let err = f.read_range(10, 0).unwrap_err();
+        println!("test_read_at_io_error: {}", err.to_string());
         assert!(err.to_string().starts_with("Bad file descriptor "));
     }
 
