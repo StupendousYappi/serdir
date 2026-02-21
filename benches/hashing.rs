c79536ca9360fd8fdf6748fea14cf5466e2a194a
Greg Steffensen <greg.steffensen@gmail.com>
2026-02-21 00:11:07 -0500

    Improved benchmark accuracy.

--- benches/hashing.rs
+++ benches/hashing.rs
@@ -2,6 +2,7 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use rapidhash::quality::RapidHasher;
 use std::collections::hash_map::DefaultHasher;
 use std::hash::Hasher;
+use std::hint::black_box;
 
 const TEST_STRING: &str = "/var/lib/app/static/index.38sx230sk30a.html";
 
@@ -12,7 +13,7 @@ fn bench_hashing(c: &mut Criterion) {
         b.iter(|| {
             let mut hasher = DefaultHasher::new();
             hasher.write(TEST_STRING.as_bytes());
-            hasher.finish()
+            black_box(hasher.finish());
         })
     });
 
@@ -20,7 +21,7 @@ fn bench_hashing(c: &mut Criterion) {
         b.iter(|| {
             let mut hasher = RapidHasher::default();
             hasher.write(TEST_STRING.as_bytes());
-            hasher.finish()
+            black_box(hasher.finish());
         })
     });
 
